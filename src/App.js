e9a7442f10ca4c6c324e516432ec727abcae141c
vakitaki <vakitaki@yandex.com>
2023-03-30 12:55:16 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Test from "./test";
 
 function App() {
   return (
     <div className="App">
-      <Test />
     </div>
   );
 }
--- src/test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import classes from "";
-
-const Test = (props) => {
-  return <div className="">Test</div>;
-};
-
-export default Test;
