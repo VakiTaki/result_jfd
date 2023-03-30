b9f978581b1ac8a3df4c3b9b882991d36cd4d196
vakitaki <vakitaki@yandex.com>
2023-03-30 12:53:36 +0300

    add test comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Test from "./test";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Test />
     </div>
   );
 }
---
+++ src/test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import classes from "";
+
+const Test = (props) => {
+  return <div className="">Test</div>;
+};
+
+export default Test;
